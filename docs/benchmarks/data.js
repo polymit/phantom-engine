bbe3e14733c7c9dc8e01e0db5ec7cd046b1502d2
github-action-benchmark <github@users.noreply.github.com>
2026-04-18 15:34:44 +0000

    add Benchmark (cargo) benchmark result for e41498c0c019f0253f9b7ccc08cd0a482e9518e6

--- docs/benchmarks/data.js
+++ docs/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776526256793,
+  "lastUpdate": 1776526484911,
   "repoUrl": "https://github.com/polymit/phantom-engine",
   "entries": {
     "Benchmark": [
@@ -923,6 +923,90 @@ window.BENCHMARK_DATA = {
             "name": "pool_acquire_tier1",
             "value": 202331,
             "range": "± 1835",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "polymit.main@gmail.com",
+            "name": "polymit",
+            "username": "polymit-hq"
+          },
+          "committer": {
+            "email": "polymit.main@gmail.com",
+            "name": "polymit",
+            "username": "polymit-hq"
+          },
+          "distinct": true,
+          "id": "e41498c0c019f0253f9b7ccc08cd0a482e9518e6",
+          "message": "fix(ci): fix yaml syntax error in metrics verification",
+          "timestamp": "2026-04-18T21:01:56+05:30",
+          "tree_id": "c29a73937b9af1d4b10131a52d79d311012c79c8",
+          "url": "https://github.com/polymit/phantom-engine/commit/e41498c0c019f0253f9b7ccc08cd0a482e9518e6"
+        },
+        "date": 1776526484601,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "session_create_quickjs",
+            "value": 596,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "session_create_v8",
+            "value": 610,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "session_clone_cow",
+            "value": 491,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "session_suspend_resume",
+            "value": 99,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cct_full_1000_nodes",
+            "value": 3245388,
+            "range": "± 18988",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cct_selective_1000_nodes",
+            "value": 3234803,
+            "range": "± 10531",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cct_delta_10_mutations",
+            "value": 5408,
+            "range": "± 19",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "quickjs_eval_simple",
+            "value": 3184,
+            "range": "± 34",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "v8_eval_simple",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "pool_acquire_tier1",
+            "value": 206470,
+            "range": "± 1346",
             "unit": "ns/iter"
           }
         ]
